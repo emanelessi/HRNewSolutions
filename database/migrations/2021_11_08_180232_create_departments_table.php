7774339e6530d6d2aa4b0e21dcbe34a965a98f61
DinaAkilaDev <dinashadiakeela@gmail.com>
2021-11-11 10:18:36 +0200

    commit

--- database/migrations/2021_11_08_180232_create_departments_table.php
+++ database/migrations/2021_11_08_180232_create_departments_table.php
@@ -16,8 +16,6 @@ class CreateDepartmentsTable extends Migration
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-//            $table->unsignedBigInteger('manager_id');
-//            $table->foreign('manager_id')->references('id')->on('employees')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
         });
