a7c33d82408246341b6195b835d7ccffbc1156b3
DinaAkilaDev <dinashadiakeela@gmail.com>
2021-11-16 12:57:11 +0200

    commit

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
-
-class Admin extends Model
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class Admin extends Authenticatable
 {
-    use HasFactory;
-    use Notifiable;
-    use SoftDeletes;
+    use HasFactory, Notifiable, HasApiTokens,SoftDeletes;
 
-    protected $guard = "admin";
+    protected $table = 'admins';
+    protected $guarded = array();
 
     protected $fillable = [
         'username', 'email', 'password',
