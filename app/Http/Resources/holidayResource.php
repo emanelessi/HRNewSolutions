246ea50af0950e5f2d93dab03e47893af17dcff6
DinaAkilaDev <dinashadiakeela@gmail.com>
2021-11-16 11:30:18 +0200

    commit

---
+++ app/Http/Resources/holidayResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class holidayResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'duration'=>$this->duration,
+            'description'=>$this->description,
+            'date'=>$this->date,
+            'type'=>$this->type,
+            'status'=>$this->status,
+            'employee'=>new employeeResource($this->user),
+
+
+        ];
+    }
+}
