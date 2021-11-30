ebeb9ee8babfca1e438a8a03b4455951544ae3b0
DinaAkilaDev <dinashadiakeela@gmail.com>
2021-11-30 09:36:02 +0200

    Merge branch 'api' # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,6 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -41,12 +43,12 @@ class User extends Authenticatable
     ];
     public function department()
     {
-        return $this->hasOne(Department::class, 'department_id');
+        return $this->belongsTo(Department::class, 'department_id');
     }
 
     public function job()
     {
-        return $this->hasMany(Job::class, 'job_id','id');
+        return $this->belongsTo(Job::class, 'job_id');
     }
 
     public function reward()
@@ -66,11 +68,11 @@ class User extends Authenticatable
 
     public function project()
     {
-        return $this->hasMany(Project::class, 'project_id', 'id');
+        return $this->belongsToMany(Project::class, 'employee_projects');
     }
 
     public function jobhistory()
     {
-        return $this->hasOne(JobHistory::class, 'employee_id');
+        return $this->hasOne(JobHistory::class, 'user_id');
     }
 }
