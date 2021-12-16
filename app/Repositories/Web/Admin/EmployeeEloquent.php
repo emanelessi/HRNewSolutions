8c519401597fe217960d98b3a53c37f402f28003
enamelessi <hanieman86@gmail.com>
2021-12-16 12:20:39 +0200

    Initial commit

--- app/Repositories/Web/Admin/EmployeeEloquent.php
+++ app/Repositories/Web/Admin/EmployeeEloquent.php
@@ -46,50 +46,50 @@ class EmployeeEloquent
         $holidays = Holiday::all();
         $jobs = Job::all();
 
-//        $chartjs= app()->chartjs
-//            ->name('lineChartTest')
-//            ->type('line')
-//            ->size(['width' => 400, 'height' => 200])
-//            ->labels(['January', 'February', 'March', 'April', 'May', 'June', 'July'])
-//            ->datasets([
-//                [
-//                    "label" => "My First dataset",
-//                    'backgroundColor' => "rgba(38, 185, 154, 0.31)",
-//                    'borderColor' => "rgba(38, 185, 154, 0.7)",
-//                    "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
-//                    "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
-//                    "pointHoverBackgroundColor" => "#fff",
-//                    "pointHoverBorderColor" => "rgba(220,220,220,1)",
-//                    'data' => [65, 59, 80, 81, 56, 55, 40],
-//                ],
-//                [
-//                    "label" => "My Second dataset",
-//                    'backgroundColor' => "rgba(38, 185, 154, 0.31)",
-//                    'borderColor' => "rgba(38, 185, 154, 0.7)",
-//                    "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
-//                    "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
-//                    "pointHoverBackgroundColor" => "#fff",
-//                    "pointHoverBorderColor" => "rgba(220,220,220,1)",
-//                    'data' => [12, 33, 44, 44, 55, 23, 40],
-//                ]
-//            ])
-//            ->options([]);
-//        $record = User::select(DB::raw("COUNT(*) as count"), DB::raw("DAYNAME(created_at) as day_name"), DB::raw("DAY(created_at) as day"))
-//            ->where('created_at', '>', Carbon::today()->subDay(6))
-//            ->groupBy('day_name','day')
-//            ->orderBy('day')
-//            ->get();
-//
-//        $data = [];
-//
-//        foreach($record as $row) {
-//            $data['label'][] = $row->day_name;
-//            $data['data'][] = (int) $row->count;
-//        }
-//
-//        $data['chart_data'] = json_encode($data);
+        $chartjs= app()->chartjs
+            ->name('lineChartTest')
+            ->type('line')
+            ->size(['width' => 400, 'height' => 200])
+            ->labels(['January', 'February', 'March', 'April', 'May', 'June', 'July'])
+            ->datasets([
+                [
+                    "label" => "My First dataset",
+                    'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                    'borderColor' => "rgba(38, 185, 154, 0.7)",
+                    "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                    "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                    "pointHoverBackgroundColor" => "#fff",
+                    "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                    'data' => [65, 59, 80, 81, 56, 55, 40],
+                ],
+                [
+                    "label" => "My Second dataset",
+                    'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                    'borderColor' => "rgba(38, 185, 154, 0.7)",
+                    "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                    "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                    "pointHoverBackgroundColor" => "#fff",
+                    "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                    'data' => [12, 33, 44, 44, 55, 23, 40],
+                ]
+            ])
+            ->options([]);
+        $record = User::select(DB::raw("COUNT(*) as count"), DB::raw("DAYNAME(created_at) as day_name"), DB::raw("DAY(created_at) as day"))
+            ->where('created_at', '>', Carbon::today()->subDay(6))
+            ->groupBy('day_name','day')
+            ->orderBy('day')
+            ->get();
 
-        return view('admin.home')->with('users', $users)->with('departments', $departments)->with('projects', $projects)->with('rewards', $rewards)->with('holidays', $holidays)->with('jobs', $jobs);
+        $data = [];
+
+        foreach($record as $row) {
+            $data['label'][] = $row->day_name;
+            $data['data'][] = (int) $row->count;
+        }
+
+        $data['chart_data'] = json_encode($data);
+
+        return view('admin.home')->with('users', $users)->with('departments', $departments)->with('projects', $projects)->with('rewards', $rewards)->with('holidays', $holidays)->with('jobs', $jobs)->with('chartjs',$chartjs);
     }
 
     public function addEmployee(array $data)
